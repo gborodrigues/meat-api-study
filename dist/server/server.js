930510881b88bb51b229e074b93cf6f3f54ac0e9
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2019-06-02 18:04:20 -0300

    Environment variables, configuration of class server and call bootstrap method to initialize the server

---
+++ dist/server/server.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const environment_1 = require("../common/environment");
+class Server {
+    initRoutes() {
+        return new Promise((resolve, reject) => {
+            try {
+                this.application = restify.createServer({
+                    name: 'meat-api',
+                    version: '1.0.0'
+                });
+                this.application.use(restify.plugins.queryParser());
+                this.application.listen(environment_1.environment.server.port, () => {
+                    resolve(this.application);
+                });
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    bootstrap() {
+        return this.initRoutes().then(() => this);
+    }
+}
+exports.Server = Server;
