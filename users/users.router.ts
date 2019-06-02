a29981ed14d4f611b83feb64a1a9fd019532409c
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2019-06-02 20:04:10 -0300

    Method post and get added to user

--- users/users.router.ts
+++ users/users.router.ts
@@ -1,11 +1,37 @@
 import { Router } from '../common/router'
 import * as restify from 'restify'
+import { User } from './users.model'
 
 class UserRouter extends Router {
 
     applyRoutes(application: restify.Server) {
-        application.get('/user', (req, resp, next) => {
-            resp.json({message: 'users router'})
+        application.get('/users', (req, resp, next) => {
+            User.find().then(users => {
+                resp.json(users)
+                return next() 
+            })
+        })
+
+        application.get('/users/:id', (req, resp, next) => {
+            User.findById(req.params.id).then(user => {
+                if (user) {
+                    resp.json(user)
+                    return next();
+                }
+
+                resp.send(404)
+                return next()
+            })
+        })
+
+        application.post('/users', (req, resp, next) => {
+            let user = new User(req.body)
+            user.save().then(user => {
+                user.password = undefined
+                resp.json(user)
+                return next()
+            })
+
         })
     }
 }
